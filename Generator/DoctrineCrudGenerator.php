b30591b5aa7ab6ec7fd36aa1369f6856182ab1f1
theodson <theodson@gmail.com>
2014-02-07 11:50:04 +1100

    Added ability to UnCamelCase words (add spaces) in Twig Templates Added dependency on forked Twig-Extensions (bgdevlab FORKED version - as original did not include un_camel_case capability). Modified the INDEX and ACTIONS templates to use new filter `un_camel_case` and existing filter `title`.
    e.g. changes 'anEntityFieldDesc' becomes 'An Entity Field Desc'
    
    Had to Override the Sensio Generator class in DoctrineCrudGenerator to add the TwigExtension awareness to the processing of the skeleton files. Looking at the logs it appears previous revision exposed the TwigEnvironment as an extention point but this no longer exists.

--- Generator/DoctrineCrudGenerator.php
+++ Generator/DoctrineCrudGenerator.php
@@ -43,6 +43,42 @@ class DoctrineCrudGenerator extends Generator
         $this->filesystem  = $filesystem;
     }
 
+    private $skeletonDirs;
+
+    /**
+     * Sets an array of directories to look for templates.
+     *
+     * The directories must be sorted from the most specific to the most
+     * directory.
+     *
+     * @param array $skeletonDirs An array of skeleton dirs
+     */
+    public function setSkeletonDirs($skeletonDirs)
+    {
+        $this->skeletonDirs = is_array($skeletonDirs) ? $skeletonDirs : array($skeletonDirs);
+    }
+
+    protected function renderFile($template, $target, $parameters)
+    {
+        if (!is_dir(dirname($target))) {
+            mkdir(dirname($target), 0777, true);
+        }
+
+        return file_put_contents($target, $this->render($template, $parameters));
+    }
+    
+    protected function render($template, $parameters)
+    {
+        $twig = new \Twig_Environment(new \Twig_Loader_Filesystem($this->skeletonDirs), array(
+            'debug'            => true,
+            'cache'            => false,
+            'strict_variables' => true,
+            'autoescape'       => false,
+        ));
+        $twig->addExtension(new \Twig_Extensions_Extension_Text);
+        return $twig->render($template, $parameters);
+    }
+    
     /**
      * Generate the CRUD controller.
      *
